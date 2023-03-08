a7818f3a0eac28a3525fbf44378e8c62faf4c3d5
Pratik Ramteke <pratikramteke222@gmail.com>
2023-03-08 12:54:02 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,49 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white py-10 flex justify-around">
       <div>
-        <img width={200} src="../public/images/logo.png" alt="" />
-        <p>Email us:support@pwskills.com</p>
-        
-        <img src="https://pwskills.com/images/iso-9001-2015.svg" alt="" />
+        <img
+          width={200}
+          src="https://pwskills.com/images/PWSkills-white.png"
+          alt=""
+        />
+        <p className='my-5'>Email us:support@pwskills.com</p>
+
+        <img
+          width={70}
+          src="https://pwskills.com/images/iso-9001-2015.svg"
+          alt=""
+        />
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold">PW Skills</h2>
+        <hr className="my-5 w-32 mx-auto border-1 bg-yellow-400 border-yellow-400 " />
+        <div className="flex gap-10">
+          <ul>
+            <li>About us</li>
+            <li>FAQs</li>
+            <li>Privacy Policy</li>
+          </ul>
+          <ul>
+            <li>Contact Us</li>
+            <li>Job Assistance</li>
+            <li>Terms and Conditions</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h2 className=" text-2xl font-bold">Products</h2>
+        <hr className="my-5 w-32 mx-auto border-1 bg-yellow-400 border-yellow-400" />
+        <div>
+          <ul>
+            <li>PW Skills Lab</li>
+            <li>Job Portal</li>
+            <li>Experience Portal</li>
+            <li>Become an Affilate</li>
+            <li>Hall of Fame</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -9,19 +9,19 @@ export default function Programs() {
       src: "https://cdn.pwskills.com/assets/uploads/thumbnails/63a2ecdd88994300d47eb9ad.jpg",
       courseName: "Full Stack Web Development",
       teacher: "Anurag Tiwari",
-      price: "$2975.00",
+      price: "₹2975.00",
     },
     {
       src: "https://cdn.pwskills.com/assets/uploads/thumbnails/63a2ea7e8899438ee77eae5a.jpg",
       courseName: "Data Science Masters",
       teacher: "Krish Naik",
-      price: "$2975.00",
+      price: "₹2975.00",
     },
     {
       src: "https://cdn.pwskills.com/assets/uploads/thumbnails/63a2f0198899433e697ec2ac.jpg",
       courseName: "Java with DSA and System Design",
       teacher: "Priya Bhatia",
-      price: "$2975.00",
+      price: "₹2975.00",
     },
   ];
 
